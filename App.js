441d09d42dc831694ae64135b4f34ac0a4046439
คณะกรรมการเกษตรแห่งชาติ30พค92 <s039@SUT.AC.TH>
2020-01-18 11:44:38 +0700

    new project

---
+++ App.js
@@ -0,0 +1,18 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import { Login } from './view/Login/Login';
+
+const App = () => {
+  return(
+    <Login btn="test"/>
+  );
+};
+export default App;
+  
---
+++ view/Login/Login.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { Image, StyleSheet, Dimensions, Alert } from 'react-native';
+import{
+    StatusBar,
+    ScrollView,
+    Text,
+    View,
+    TextInput,
+    TouchableOpacity,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+  const width = Dimensions.get('window').width;
+  const height = Dimensions.get('window').height;   
+  
+export class Login extends React.Component {
+ constructor(props) {
+      super(props)
+      this.state = {
+          username: 'admin',
+          password: '123456'
+      }
+ }
+ componentDidMount(){
+    this._getLogin("Fang")
+ }
+
+_getLogin(param = 'test'){
+    Alert.alert("Function", "_getLogin " + param)
+}
+
+ render(){
+            return (
+                <ScrollView style={{ backgroundColor: "#010001", }}>
+                     <StatusBar hidden={true}/>
+                    <View style={{ padding: 36, }}>
+                        <Image  resizeMode="contain" source={require('../../images/logo.png')} style={{width:width/3,height:height/3}}></Image>
+                        <View style={[ styles.row_underline,{ marginBottom:16, }]}>
+                            <Icon name="email-outline" style={styles.login_Icon}/>
+                            <TextInput placeholder="Email address"
+                                placeholderTextColor="#fff"
+                                underlineColorAndroid='transparent'
+                                style={{ color:"#fff",flex:1,fontSize:16, paddingLeft: 12,}}
+                                value = {this.state.username}
+                            />
+                        </View>
+                        <View style={[ styles.row_underline,{ marginBottom:24, }]}>
+                            <Icon name="lock-outline" style={styles.login_Icon}/>
+                            <TextInput placeholder="Password"
+                                placeholderTextColor="#fff"
+                                underlineColorAndroid='transparent'
+                                style={{ color:"#fff",flex:1,fontSize:16, paddingLeft: 12,}}
+                                secureTextEntry={true}
+                                value = {this.state.password}
+                            />
+                        </View>
+                        <TouchableOpacity style={{ flex:1, backgroundColor:"#1B70E3", borderRadius: 2 ,paddingLeft: 10,}} onPress={() => this._getLogin()}>
+                        <Text style={{ alignSelf: "center",fontSize: 16 , color:"#fff", }} >Login</Text>
+                        </TouchableOpacity>
+                    </View>
+                </ScrollView>
+
+            
+            );
+ }
+
+}
+
+const styles = StyleSheet.create({
+    row_underline: {
+      flexDirection: "row",
+      borderBottomWidth: 1,
+      borderBottomColor: "#b6b6b6",
+    },
+    login_Icon:{
+        alignSelf: "center",
+        fontSize: 20,
+        color: "#DF9808",
+    }
+  });
