5fb5c461b6ac067bb7888000865634f55f6d702d
TheSunSkys <42728873+TheSunSkys@users.noreply.github.com>
2020-01-25 15:47:49 +0700

    add async storage

--- App.js
+++ App.js
@@ -1,60 +1,61 @@
 import {
-  createAppContainer,
-  createSwitchNavigator,
-} from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack'
-
-import { Login } from './views/Login/Login'
-import { Home } from './views/Home/Home'
-import { NewsDetail } from './views/News/NewsDetail'
-import { ShopDetail } from './views/Shop/ShopDetail'
-
-const HomeNavigation = createStackNavigator({
-  Home: { 
+    createAppContainer,
+    createSwitchNavigator,
+  } from 'react-navigation';
+  import { createStackNavigator } from 'react-navigation-stack'
+  
+  import { Login } from './views/Login/Login'
+  import { Home } from './views/Home/Home'
+  import { NewsDetail } from './views/News/NewsDetail'
+  import { ShopDetail } from './views/Shop/ShopDetail'
+  
+  const HomeNavigation = createStackNavigator({
+    Home: { 
       screen: Home,
       navigationOptions: {
-          header: null
+        header: null
       }
-  },
-  NewsDetail: { 
+    },
+    NewsDetail: { 
       screen: NewsDetail,
       navigationOptions: {
-          title: 'รายละเอียด',
-          headerTintColor: '#f3f3f3',
-          headerTitleStyle: {
-              fontSize: 18,
-              fontFamily: 'Kanit-Regular',
-              color: '#f3f3f3'
-          },
-          headerStyle: {
-              backgroundColor: '#010001',
-          },
+        title: 'รายละเอียด',
+        headerTintColor: '#f3f3f3',
+        headerTitleStyle: {
+          fontSize: 18,
+          fontFamily: 'Kanit-Regular',
+          color: '#f3f3f3'
+        },
+        headerStyle: {
+          backgroundColor: '#010001',
+        },
       }
-  },
-  ShopDetail: { 
+    },
+    ShopDetail: { 
       screen: ShopDetail, 
       navigationOptions: {
-          title: 'รายละเอียด',
-          headerTintColor: '#f3f3f3',
-          headerTitleStyle: {
-              fontSize: 18,
-              fontWeight: '400',
-              fontFamily: 'Kanit-Regular',
-              color: '#f3f3f3'
-          },
-          headerStyle: {
-              backgroundColor: '#010001',
-          },
+        title: 'รายละเอียด',
+        headerTintColor: '#f3f3f3',
+        headerTitleStyle: {
+          fontSize: 18,
+          fontWeight: '400',
+          fontFamily: 'Kanit-Regular',
+          color: '#f3f3f3'
+        },
+        headerStyle: {
+          backgroundColor: '#010001',
+        },
       }
-  },
-}, {
-  initialRouteParams: 'Home',
-});
-
-const Navigation = createSwitchNavigator({
-  Home: HomeNavigation,
-})
-
-const App = createAppContainer(Navigation);
-
-export default App;
+    },
+  }, {
+    initialRouteParams: 'Home',
+  });
+  
+  const Navigation = createSwitchNavigator({
+    Login: Login,
+    Home: HomeNavigation,
+  })
+  
+  const App = createAppContainer(Navigation);
+  
+  export default App;
