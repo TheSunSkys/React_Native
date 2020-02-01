d14093a07f31efd36c978021774978bd54c01cb5
TheSunSkys <42728873+TheSunSkys@users.noreply.github.com>
2020-02-01 14:22:21 +0700

    edit

--- android/app/src/main/java/com/revel_market_app/MainApplication.java
+++ android/app/src/main/java/com/revel_market_app/MainApplication.java
@@ -26,7 +26,6 @@ public class MainApplication extends Application implements ReactApplication {
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
 
-          packages.add(new RNCameraKitPackage())
           return packages;
         }
 
